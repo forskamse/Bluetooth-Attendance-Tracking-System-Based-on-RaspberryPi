c6a967c7fe7a7203869f60f4df98e091dc18c675
Forskamse <33803970+forskamse@users.noreply.github.com>
2019-03-14 17:58:28 +0800

    Update auto-plot-and-send-working-hours.sh

--- auto-plot-and-send-working-hours.sh
+++ auto-plot-and-send-working-hours.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/pi/projects/
-/home/pi/.virtualenvs/bt/bin/python /home/pi/projects/plot-working-hours.py
-sendEmail -f sender_email_address -t receiver_email_address -u "Working Hours from RaspberryPi" -m "Working hours of $(date --date '1 days ago' +%Y-%m-%d)" -a /home/pi/projects/working-hours.png -s sender_email_smtp_server_address  -o tls=yes -xu sender_email_address -xp sender_email_password
+cd ~/RaspberryPi-Bluetooth-Punch-System/
+python3 ~/RaspberryPi-Bluetooth-Punch-System/plot-working-hours.py
+sendEmail -f sender_email_address -t receiver_email_address -u "Working Hours from RaspberryPi" -m "Working hours of $(date --date '1 days ago' +%Y-%m-%d)" -a ~/RaspberryPi-Bluetooth-Punch-System/working-hours.png -s sender_email_smtp_server_address  -o tls=yes -xu sender_email_address -xp sender_email_password
